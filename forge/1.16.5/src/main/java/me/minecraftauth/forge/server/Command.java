5ff6a0560df7fc022dc80f487a2e53ae38b5181f
Austin Shapiro <truescarsz@gmail.com>
2021-12-27 07:11:49 -0800

    clean up forge 1.16.5 command permission requirement

--- forge/1.16.5/src/main/java/me/minecraftauth/forge/server/Command.java
+++ forge/1.16.5/src/main/java/me/minecraftauth/forge/server/Command.java
@@ -21,38 +21,33 @@ import com.mojang.brigadier.context.CommandContext;
 import github.scarsz.configuralize.ParseException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 import java.io.IOException;
 
 public class Command {
 
     public Command(CommandDispatcher<CommandSource> dispatcher) {
-        dispatcher.register(Commands.literal("minecraftauth").then(Commands.literal("reload").executes(this::reload)));
+        dispatcher.register(Commands.literal("minecraftauth")
+                .then(Commands.literal("reload")
+                        .requires(cs -> cs.hasPermission(3))
+                        .executes(context -> reload(context, context.getSource()))
+                )
+        );
     }
 
-    private int reload(CommandContext<CommandSource> context) {
-        CommandSource source = context.getSource();
-        Entity entity = source.getEntity();
-
-        if (entity == null || (entity instanceof PlayerEntity && ServerLifecycleHooks.getCurrentServer().getPlayerList().isOp(((PlayerEntity) entity).getGameProfile()))) {
-            try {
-                MinecraftAuthMod.getInstance().getService().fullReload();
-                source.sendSuccess(new StringTextComponent("MinecraftAuth config reloaded").withStyle(TextFormatting.RED), true);
-                return 1;
-            } catch (IOException e) {
-                source.sendFailure(new StringTextComponent("IO exception while reading config: " + e.getMessage()).withStyle(TextFormatting.RED));
-                e.printStackTrace();
-            } catch (ParseException e) {
-                source.sendFailure(new StringTextComponent("Exception while parsing config: " + e.getMessage()).withStyle(TextFormatting.RED));
-                e.printStackTrace();
-            }
-        } else {
-            source.sendFailure(new StringTextComponent("Server operator-only command").withStyle(TextFormatting.RED));
+    private int reload(CommandContext<CommandSource> context, CommandSource source) {
+        try {
+            MinecraftAuthMod.getInstance().getService().fullReload();
+            source.sendSuccess(new StringTextComponent("MinecraftAuth config reloaded").withStyle(TextFormatting.RED), true);
+            return 1;
+        } catch (IOException e) {
+            source.sendFailure(new StringTextComponent("IO exception while reading config: " + e.getMessage()).withStyle(TextFormatting.RED));
+            e.printStackTrace();
+        } catch (ParseException e) {
+            source.sendFailure(new StringTextComponent("Exception while parsing config: " + e.getMessage()).withStyle(TextFormatting.RED));
+            e.printStackTrace();
         }
 
         return -1;
